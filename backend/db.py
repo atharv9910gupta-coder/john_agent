feeafff7965aa6a9581e04d2e677ce9cbccb2912
atharv9910gupta-coder <atharv9910gupta@gmail.com>
2025-12-12 18:49:21 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,13 @@
+# backend/db.py
+from sqlmodel import SQLModel, create_engine, Session
+from backend.config import settings
+
+engine = create_engine(settings.DATABASE_URL, echo=False, pool_pre_ping=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
